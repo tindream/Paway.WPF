ba76a9bed311066b2a94af1d051dd0987083415f
zfz <tindream02@126.com>
2020-06-09 14:22:49 +0800

    调整Window扩展按钮内外边框

--- Paway.WPF/Controls/Extended/ButtonPath.cs
+++ Paway.WPF/Controls/Extended/ButtonPath.cs
@@ -13,18 +13,31 @@ namespace Paway.WPF
     /// </summary>
     public partial class ButtonPath : Button
     {
+        #region 依赖属性
+        /// <summary>
+        /// </summary>
         public static readonly DependencyProperty ValueProperty =
                 DependencyProperty.RegisterAttached(nameof(Value), typeof(double), typeof(ButtonPath), new PropertyMetadata(0d, OnValueChanged));
+        /// <summary>
+        /// </summary>
         public static readonly DependencyProperty BorderColorProperty =
             DependencyProperty.RegisterAttached(nameof(BorderColor), typeof(Brush), typeof(ButtonPath),
             new PropertyMetadata(new SolidColorBrush(Color.FromArgb(255, 200, 200, 200))));
+        /// <summary>
+        /// </summary>
         public static readonly DependencyProperty BorderFocusedColorProperty =
             DependencyProperty.RegisterAttached(nameof(BorderFocusedColor), typeof(Brush), typeof(ButtonPath),
             new PropertyMetadata(new SolidColorBrush(Color.FromArgb(255, 35, 175, 255))));
+        /// <summary>
+        /// </summary>
         public static readonly DependencyProperty IWarnProperty =
             DependencyProperty.RegisterAttached(nameof(IWarn), typeof(bool), typeof(ButtonPath));
+        /// <summary>
+        /// </summary>
         public static readonly DependencyProperty IEmptyProperty =
             DependencyProperty.RegisterAttached(nameof(IEmpty), typeof(bool), typeof(ButtonPath));
+
+        #endregion
 
         private static void OnValueChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
         {
@@ -34,6 +47,10 @@ namespace Paway.WPF
             }
         }
 
+        #region 扩展
+        /// <summary>
+        /// 内部进度条的值
+        /// </summary>
         [Category("扩展")]
         [Description("内部进度条的值")]
         public double Value
@@ -41,6 +58,9 @@ namespace Paway.WPF
             get { return (double)GetValue(ValueProperty); }
             set { SetValue(ValueProperty, value); }
         }
+        /// <summary>
+        /// 默认的边框颜色
+        /// </summary>
         [Category("扩展")]
         [Description("默认的边框颜色")]
         public Brush BorderColor
@@ -48,6 +68,9 @@ namespace Paway.WPF
             get { return (Brush)GetValue(BorderColorProperty); }
             set { SetValue(BorderColorProperty, value); }
         }
+        /// <summary>
+        /// 鼠标移过时的边框颜色
+        /// </summary>
         [Category("扩展")]
         [Description("鼠标移过时的边框颜色")]
         public Brush BorderFocusedColor
@@ -55,6 +78,9 @@ namespace Paway.WPF
             get { return (Brush)GetValue(BorderFocusedColorProperty); }
             set { SetValue(BorderFocusedColorProperty, value); }
         }
+        /// <summary>
+        /// 余量低报警
+        /// </summary>
         [Browsable(false)]
         [Description("余量低报警")]
         public bool IWarn
@@ -62,6 +88,9 @@ namespace Paway.WPF
             get { return (bool)GetValue(IWarnProperty); }
             set { SetValue(IWarnProperty, value); }
         }
+        /// <summary>
+        /// 余量空报警
+        /// </summary>
         [Browsable(false)]
         [Description("余量空报警")]
         public bool IEmpty
@@ -70,6 +99,10 @@ namespace Paway.WPF
             set { SetValue(IEmptyProperty, value); }
         }
 
+        #endregion
+
+        /// <summary>
+        /// </summary>
         public ButtonPath()
         {
             DefaultStyleKey = typeof(ButtonPath);
