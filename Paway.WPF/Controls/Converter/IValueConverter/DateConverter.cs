3c0c972ff2724ff7e922469660d4ae561f734fce
tinn <tindream02@126.com>
2020-06-28 16:00:32 +0800

    1.1.0
    添加DateConverter格式转换

---
+++ Paway.WPF/Controls/Converter/IValueConverter/DateConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Paway.WPF
+{
+    /// <summary>
+    /// DateTime格式化
+    /// </summary>
+    internal class DateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime time)
+            {
+                if (time == DateTime.MinValue) return null;
+                var param = parameter == null ? "g" : parameter.ToString();
+                return time.ToString(param);
+            }
+            throw new NotImplementedException();
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
