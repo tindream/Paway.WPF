937e795af96937a7f41327c30e985a855ae4e9ce
tinn <tindream02@126.com>
2020-07-07 22:06:33 +0800

    1.1.5

--- Paway.WPF/Properties/AssemblyInfo.cs
+++ Paway.WPF/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("Paway.WPF")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("WPF control extension")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Paway.WPF")]
