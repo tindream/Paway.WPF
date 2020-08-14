cd7e1299952c74dab59d6243df458f91e418fa26
zfz <tindream02@126.com>
2020-08-14 17:47:14 +0800

    测试调整

--- Paway.WPF/Controls/Converter/IValueConverter/DateConvertToColor.cs
+++ Paway.WPF/Controls/Converter/IValueConverter/DateConvertToColor.cs
@@ -16,11 +16,12 @@ namespace Paway.WPF
         public object Convert(object values, Type targetType, object parameter, CultureInfo culture)
         {
             var calendarDayButton = (CalendarDayButton)values;
-            var dateTime = (DateTime)calendarDayButton.DataContext;
-            if (!calendarDayButton.IsMouseOver && !calendarDayButton.IsSelected && !calendarDayButton.IsBlackedOut && (dateTime.DayOfWeek == DayOfWeek.Saturday || dateTime.DayOfWeek == DayOfWeek.Sunday))
-                return new SolidColorBrush(Color.FromArgb(255, 255, 47, 47));
-            else
-                return new SolidColorBrush(Color.FromArgb(255, 51, 51, 51));
+            if (calendarDayButton.DataContext is DateTime dateTime)
+            {
+                if (!calendarDayButton.IsMouseOver && !calendarDayButton.IsSelected && !calendarDayButton.IsBlackedOut && (dateTime.DayOfWeek == DayOfWeek.Saturday || dateTime.DayOfWeek == DayOfWeek.Sunday))
+                    return new SolidColorBrush(Color.FromArgb(255, 255, 47, 47));
+            }
+            return new SolidColorBrush(Color.FromArgb(255, 51, 51, 51));
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
