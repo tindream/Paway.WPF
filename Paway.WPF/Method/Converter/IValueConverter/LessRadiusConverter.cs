752ae27ccb9b3ff3b7027f6618a72a06d58b7b1b
zfz <9030140@qq.com>
2021-11-01 15:47:40 +0800

    样式微调(#273)

---
+++ Paway.WPF/Method/Converter/IValueConverter/LessRadiusConverter.cs
@@ -0,0 +1,26 @@
+﻿using Paway.Helper;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Paway.WPF
+{
+    internal class LessRadiusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is CornerRadius radius)
+            {
+                return new CornerRadius(Math.Max(0, radius.TopLeft - 1), Math.Max(0, radius.TopRight - 1), Math.Max(0, radius.BottomRight - 1), Math.Max(0, radius.BottomLeft - 1));
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
