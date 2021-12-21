fcd816b4d63498cef266a2500805eba6fca648e8
zfz <9030140@qq.com>
2021-12-21 14:49:00 +0800

    TabControlEXT添加菜单样式；添加RadioBox按钮样式(同CheckButton)(#327)

---
+++ Paway.WPF/Controls/Extended/RadioBox.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Paway.WPF
+{
+    /// <summary>
+    /// RadioButton扩展-按钮样式
+    /// </summary>
+    public partial class RadioBox : RadioButton
+    {
+        #region 依赖属性
+        /// <summary>
+        /// </summary>
+        public static readonly DependencyProperty RadiusProperty =
+            DependencyProperty.RegisterAttached(nameof(Radius), typeof(CornerRadius), typeof(RadioBox), new PropertyMetadata(new CornerRadius(3)));
+        /// <summary>
+        /// </summary>
+        public static readonly DependencyProperty ItemBorderProperty =
+            DependencyProperty.RegisterAttached(nameof(ItemBorder), typeof(ThicknessEXT), typeof(RadioBox), new PropertyMetadata(new ThicknessEXT(1, 0)));
+        /// <summary>
+        /// </summary>
+        public static readonly DependencyProperty ItemBrushProperty =
+            DependencyProperty.RegisterAttached(nameof(ItemBrush), typeof(BrushEXT), typeof(RadioBox),
+                new PropertyMetadata(new BrushEXT()));
+        /// <summary>
+        /// </summary>
+        public static readonly DependencyProperty ItemForegroundProperty =
+            DependencyProperty.RegisterAttached(nameof(ItemForeground), typeof(BrushEXT), typeof(RadioBox),
+            new PropertyMetadata(new BrushEXT(PConfig.TextColor, Colors.White, Colors.White)));
+        /// <summary>
+        /// </summary>
+        public static readonly DependencyProperty TypeProperty =
+            DependencyProperty.RegisterAttached(nameof(Type), typeof(ColorType), typeof(RadioBox),
+            new UIPropertyMetadata(ColorType.Color, OnColorTypeChanged));
+        private static void OnColorTypeChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            if (obj is RadioBox view)
+            {
+                if (view.Type != ColorType.None)
+                {
+                    var color = view.Type.Color();
+                    view.ItemBrush = new BrushEXT(PMethod.AlphaColor(PConfig.Alpha, color))
+                    {
+                        Normal = new SolidColorBrush(Colors.LightGray)
+                    };
+                }
+                view.UpdateDefaultStyle();
+            }
+        }
+
+        #endregion
+
+        #region 扩展
+        /// <summary>
+        /// 自定义边框圆角
+        /// </summary>
+        [Category("扩展")]
+        [Description("自定义边框圆角")]
+        public CornerRadius Radius
+        {
+            get { return (CornerRadius)GetValue(RadiusProperty); }
+            set { SetValue(RadiusProperty, value); }
+        }
+        /// <summary>
+        /// 自定义边框线
+        /// </summary>
+        [Category("扩展")]
+        [Description("自定义边框线")]
+        public ThicknessEXT ItemBorder
+        {
+            get { return (ThicknessEXT)GetValue(ItemBorderProperty); }
+            set { SetValue(ItemBorderProperty, value); }
+        }
+        /// <summary>
+        /// 自定义文本颜色
+        /// </summary>
+        [Category("扩展")]
+        [Description("自定义文本颜色")]
+        public BrushEXT ItemForeground
+        {
+            get { return (BrushEXT)GetValue(ItemForegroundProperty); }
+            set { SetValue(ItemForegroundProperty, value); }
+        }
+        /// <summary>
+        /// 边框颜色
+        /// </summary>
+        [Category("扩展")]
+        [Description("边框颜色")]
+        public BrushEXT ItemBrush
+        {
+            get { return (BrushEXT)GetValue(ItemBrushProperty); }
+            set { SetValue(ItemBrushProperty, value); }
+        }
+        /// <summary>
+        /// 颜色样式
+        /// </summary>
+        [Category("扩展")]
+        [Description("颜色样式")]
+        public ColorType Type
+        {
+            get { return (ColorType)GetValue(TypeProperty); }
+            set { SetValue(TypeProperty, value); }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// </summary>
+        public RadioBox()
+        {
+            DefaultStyleKey = typeof(RadioBox);
+        }
+    }
+}
