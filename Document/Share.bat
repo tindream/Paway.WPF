69559b10c0f42054a92b7c529fb614202fc12ff0
zfz <9030140@qq.com>
2022-05-17 11:36:54 +0800

    更新引用(#444)

--- Document/Share.bat
+++ Document/Share.bat
@@ -1,2 +1,2 @@
-mklink /j Paway.Helper.2.4.5  ..\packages\Paway.Helper.2.4.5.5\lib\net45
-mklink /j Paway.Utils.2.4.5  ..\packages\Paway.Utils.2.4.5.1\lib\net46
+mklink /j Paway.Helper.2.4.6  ..\packages\Paway.Helper.2.4.6\lib\net45
+mklink /j Paway.Utils.2.4.6  ..\packages\Paway.Utils.2.4.6\lib\net46
