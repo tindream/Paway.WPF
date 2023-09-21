d7daa8a440ac13631cdab9457afa1ebcf6bd0f0a
zfz <9030140@qq.com>
2023-09-21 13:53:06 +0800

    单元测试(#616)

---
+++ Paway.Test/Test/PMethodTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Paway.WPF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paway.Test.Tests
+{
+    [TestClass()]
+    public class PMethodTests
+    {
+        [TestMethod()]
+        [Timeout(15000)]
+        public void Test1()
+        {
+            Assert.Fail();
+        }
+    }
+}
