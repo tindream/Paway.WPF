58643922b162b0dbef46db08c3b2708b23e25850
tinn <tindream02@126.com>
2023-12-12 22:12:15 +0800

    迁移项目Paway.Comm(#634)

--- builder.cmd
+++ builder.cmd
@@ -7,13 +7,8 @@
 @echo ---------------reactor-----------------------------------------------------
 @dotNET_Reactor -project builder\Paway.WPF_45.nrproj
 @copy bin\Release\net45\Paway.WPF.xml		bin\Release\net45\Paway.WPF_Secure\Paway.WPF.xml
-@dotNET_Reactor -project builder\Paway.Comm_452.nrproj
-@copy bin\Release\net452\Paway.Comm.xml		bin\Release\net452\Paway.Comm_Secure\Paway.Comm.xml
-@dotNET_Reactor -project builder\Paway.Comm_core3.1.nrproj
-@copy bin\Release\netcoreapp3.1\Paway.Comm.xml	bin\Release\netcoreapp3.1\Paway.Comm_Secure\Paway.Comm.xml
 @echo ---------------------------------------------------------------------------
 @echo ---------------nugut------------------------------------------------------
 @nuget pack builder\Paway.WPF.nuspec
-@nuget pack builder\Paway.Comm.nuspec
 @echo --------------------------------------------------------------------------- 
 @IF "%1" == "" @PAUSE
