bc3d80489f681038a8ab989e6f18e3e6a663ae13
zfz <9030140@qq.com>
2023-12-15 13:45:15 +0800

    发布NG(###########################################1.3.7.640)

--- AssemblyVersion.tpl
+++ AssemblyVersion.tpl
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.3.6.0")]
-[assembly: AssemblyFileVersion("1.3.6.{Version}")]
-[assembly: AssemblyInformationalVersion("1.3.6.0")]
+[assembly: AssemblyVersion("1.3.7.0")]
+[assembly: AssemblyFileVersion("1.3.7.{Version}")]
+[assembly: AssemblyInformationalVersion("1.3.7.0")]
--- Document/Share.bat
+++ Document/Share.bat
@@ -1,3 +1,3 @@
-mklink /j Paway.Helper.2.5.1  C:\Users\Tinn\.nuget\packages\paway.helper\2.5.1\lib\net45
-mklink /j Paway.Utils.2.5.1  C:\Users\Tinn\.nuget\packages\paway.utils\2.5.1\lib\net45
-mklink /j Paway.Comm.2.5.1  C:\Users\Tinn\.nuget\packages\paway.comm\2.5.1\lib\net452
+mklink /j Paway.Helper.2.5.2  C:\Users\Tinn\.nuget\packages\paway.helper\2.5.2\lib\net45
+mklink /j Paway.Utils.2.5.2  C:\Users\Tinn\.nuget\packages\paway.utils\2.5.2\lib\net45
+mklink /j Paway.Comm.2.5.2  C:\Users\Tinn\.nuget\packages\paway.comm\2.5.2\lib\net452
