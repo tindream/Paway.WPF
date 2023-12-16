6f79e4fe2169204c6c9d5d45a9c31c54b002b4f7
tinn <tindream02@126.com>
2023-12-16 11:58:25 +0800

    发布NG(###########################################1.3.8.643)

--- builder.cmd
+++ builder.cmd
@@ -15,12 +15,12 @@
 @nuget pack builder\Paway.WPF.nuspec
 @nuget pack builder\Paway.Model.nuspec
 @echo ---------------copy-------------------------------------------------------
-@copy bin\Release\net45\Paway.WPF_Secure\Paway.WPF.dll		builder\Paway.WPF.1.3.7_net45\Paway.WPF.dll
-@copy bin\Release\net45\Paway.WPF_Secure\Paway.WPF.pdb		builder\Paway.WPF.1.3.7_net45\Paway.WPF.pdb
-@copy bin\Release\net45\Paway.WPF_Secure\Paway.WPF.xml		builder\Paway.WPF.1.3.7_net45\Paway.WPF.xml
+@copy bin\Release\net45\Paway.WPF_Secure\Paway.WPF.dll		builder\Paway.WPF.1.3.8_net45\Paway.WPF.dll
+@copy bin\Release\net45\Paway.WPF_Secure\Paway.WPF.pdb		builder\Paway.WPF.1.3.8_net45\Paway.WPF.pdb
+@copy bin\Release\net45\Paway.WPF_Secure\Paway.WPF.xml		builder\Paway.WPF.1.3.8_net45\Paway.WPF.xml
 
-@copy bin\Release\net452\Paway.Model_Secure\Paway.Model.dll	builder\Paway.Model.1.3.7_net452\Paway.Model.dll
-@copy bin\Release\net452\Paway.Model_Secure\Paway.Model.xml	builder\Paway.Model.1.3.7_net452\Paway.Model.xml
+@copy bin\Release\net452\Paway.Model_Secure\Paway.Model.dll	builder\Paway.Model.1.3.8_net452\Paway.Model.dll
+@copy bin\Release\net452\Paway.Model_Secure\Paway.Model.xml	builder\Paway.Model.1.3.8_net452\Paway.Model.xml
 
 @copy Root\Spire.Office7.9.0\net40\Spire.Pdf.dll			builder\Spire.Pdf.7.9.0_net40\Spire.Pdf.dll
 @copy Root\Spire.Office7.9.0\netcoreapp2.0\Spire.Pdf.dll		builder\Spire.Pdf.7.9.0_netcoreapp2.0\Spire.Pdf.dll
