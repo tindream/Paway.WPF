31c8b53cce080760b9500f4d51eda3177658a0b8
Tinn <tindream02@126.com>
2024-06-10 22:46:23 +0800

    1.5.0(#720)

--- builder/Share.bat
+++ builder/Share.bat
@@ -1,3 +1,3 @@
-mklink /j Paway.WPF.1.4.1_net45							C:\Users\Tinn\.nuget\packages\paway.wpf\1.4.1\lib\net45
+mklink /j Paway.WPF.1.5.0_net45							C:\Users\Tinn\.nuget\packages\paway.wpf\1.5.0\lib\net45
 
-mklink /j Paway.Model.1.4.1_net452						C:\Users\Tinn\.nuget\packages\paway.model\1.4.1\lib\net452
+mklink /j Paway.Model.1.5.0_net452						C:\Users\Tinn\.nuget\packages\paway.model\1.5.0\lib\net452
