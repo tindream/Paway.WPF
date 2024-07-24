2ac05c178287e373cd6b5fdc62f6a8fa16254481
Tinn <tindream02@126.com>
2024-07-24 09:48:57 +0800

    1.5.1(#745)

--- builder.cmd
+++ builder.cmd
@@ -7,6 +7,8 @@
 @echo ---------------reactor-----------------------------------------------------
 @dotNET_Reactor -project builder\Paway.WPF_45.nrproj
 @copy bin\Release\net45\Paway.WPF.xml						bin\Release\net45\Paway.WPF_Secure\Paway.WPF.xml
+@dotNET_Reactor -project builder\Paway.WPF_core3.1.nrproj
+@copy bin\Release\netcoreapp3.1\Paway.WPF.xml				bin\Release\netcoreapp3.1\Paway.WPF_Secure\Paway.WPF.xml
 
 @dotNET_Reactor -project builder\Paway.Model_452.nrproj
 @copy bin\Release\net452\Paway.Model.xml					bin\Release\net452\Paway.Model_Secure\Paway.Model.xml
@@ -16,16 +18,16 @@
 @nuget pack builder\Paway.WPF.nuspec
 @nuget pack builder\Paway.Model.nuspec
 @echo ---------------copy-------------------------------------------------------
-@copy bin\Release\net45\Paway.WPF_Secure\Paway.WPF.dll					builder\Paway.WPF.1.5.0_net45\Paway.WPF.dll
-@copy bin\Release\net45\Paway.WPF_Secure\Paway.WPF.pdb					builder\Paway.WPF.1.5.0_net45\Paway.WPF.pdb
-@copy bin\Release\net45\Paway.WPF_Secure\Paway.WPF.xml					builder\Paway.WPF.1.5.0_net45\Paway.WPF.xml
-@copy bin\Release\netcoreapp3.1\Paway.WPF_Secure\Paway.WPF.dll			builder\Paway.WPF.1.5.0_netcoreapp3.1\Paway.WPF.dll
-@copy bin\Release\netcoreapp3.1\Paway.WPF_Secure\Paway.WPF.pdb			builder\Paway.WPF.1.5.0_netcoreapp3.1\Paway.WPF.pdb
-@copy bin\Release\netcoreapp3.1\Paway.WPF_Secure\Paway.WPF.xml			builder\Paway.WPF.1.5.0_netcoreapp3.1\Paway.WPF.xml
+@copy bin\Release\net45\Paway.WPF_Secure\Paway.WPF.dll					builder\Paway.WPF.1.5.1_net45\Paway.WPF.dll
+@copy bin\Release\net45\Paway.WPF_Secure\Paway.WPF.pdb					builder\Paway.WPF.1.5.1_net45\Paway.WPF.pdb
+@copy bin\Release\net45\Paway.WPF_Secure\Paway.WPF.xml					builder\Paway.WPF.1.5.1_net45\Paway.WPF.xml
+@copy bin\Release\netcoreapp3.1\Paway.WPF_Secure\Paway.WPF.dll			builder\Paway.WPF.1.5.1_netcoreapp3.1\Paway.WPF.dll
+@copy bin\Release\netcoreapp3.1\Paway.WPF_Secure\Paway.WPF.pdb			builder\Paway.WPF.1.5.1_netcoreapp3.1\Paway.WPF.pdb
+@copy bin\Release\netcoreapp3.1\Paway.WPF_Secure\Paway.WPF.xml			builder\Paway.WPF.1.5.1_netcoreapp3.1\Paway.WPF.xml
 
-@copy bin\Release\net452\Paway.Model_Secure\Paway.Model.dll				builder\Paway.Model.1.5.0_net452\Paway.Model.dll
-@copy bin\Release\net452\Paway.Model_Secure\Paway.Model.pdb				builder\Paway.Model.1.5.0_net452\Paway.Model.pdb
-@copy bin\Release\net452\Paway.Model_Secure\Paway.Model.xml				builder\Paway.Model.1.5.0_net452\Paway.Model.xml
+@copy bin\Release\net452\Paway.Model_Secure\Paway.Model.dll				builder\Paway.Model.1.5.1_net452\Paway.Model.dll
+@copy bin\Release\net452\Paway.Model_Secure\Paway.Model.pdb				builder\Paway.Model.1.5.1_net452\Paway.Model.pdb
+@copy bin\Release\net452\Paway.Model_Secure\Paway.Model.xml				builder\Paway.Model.1.5.1_net452\Paway.Model.xml
 
 @echo --------------------------------------------------------------------------- 
 @IF "%1" == "" @PAUSE
