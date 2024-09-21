e3b2941740f9794470136947d7571c5c1b6897b0
Tinn <tindream02@126.com>
2024-09-21 15:29:57 +0800

    1.5.3(#767)

--- builder/Share.bat
+++ builder/Share.bat
@@ -1,4 +1,4 @@
-mklink /j Paway.WPF.1.5.2_net45							C:\Users\Tinn\.nuget\packages\paway.wpf\1.5.2\lib\net45
-mklink /j Paway.WPF.1.5.2_netcoreapp3.1					C:\Users\Tinn\.nuget\packages\paway.wpf\1.5.2\lib\netcoreapp3.1
+mklink /j Paway.WPF.1.5.3_net45							C:\Users\Tinn\.nuget\packages\paway.wpf\1.5.3\lib\net45
+mklink /j Paway.WPF.1.5.3_netcoreapp3.1					C:\Users\Tinn\.nuget\packages\paway.wpf\1.5.3\lib\netcoreapp3.1
 
-mklink /j Paway.Model.1.5.2_net452						C:\Users\Tinn\.nuget\packages\paway.model\1.5.2\lib\net452
+mklink /j Paway.Model.1.5.3_net452						C:\Users\Tinn\.nuget\packages\paway.model\1.5.3\lib\net452
