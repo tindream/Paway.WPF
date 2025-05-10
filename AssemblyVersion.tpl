9b761f857137f21a4e3abe70d7387d199b5afc25
Tinn <tindream02@126.com>
2025-05-10 13:47:18 +0800

    1.5.9(#813)

--- AssemblyVersion.tpl
+++ AssemblyVersion.tpl
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.5.8.0")]
-[assembly: AssemblyFileVersion("1.5.8.{Version}")]
-[assembly: AssemblyInformationalVersion("1.5.8.0")]
+[assembly: AssemblyVersion("1.5.9.0")]
+[assembly: AssemblyFileVersion("1.5.9.{Version}")]
+[assembly: AssemblyInformationalVersion("1.5.9.0")]
 
 [assembly: AssemblyCompany("Tinn")]
 [assembly: AssemblyTrademark("Paway")]
--- builder/Share.bat
+++ builder/Share.bat
@@ -1,4 +1,4 @@
-mklink /j Paway.WPF.1.5.8_net45							C:\Users\Tinn\.nuget\packages\paway.wpf\1.5.8\lib\net45
-mklink /j Paway.WPF.1.5.8_netcoreapp3.1					C:\Users\Tinn\.nuget\packages\paway.wpf\1.5.8\lib\netcoreapp3.1
+mklink /j Paway.WPF.1.5.9_net45							C:\Users\Tinn\.nuget\packages\paway.wpf\1.5.9\lib\net45
+mklink /j Paway.WPF.1.5.9_netcoreapp3.1					C:\Users\Tinn\.nuget\packages\paway.wpf\1.5.9\lib\netcoreapp3.1
 
-mklink /j Paway.Model.1.5.8_net452						C:\Users\Tinn\.nuget\packages\paway.model\1.5.8\lib\net452
+mklink /j Paway.Model.1.5.9_net452						C:\Users\Tinn\.nuget\packages\paway.model\1.5.9\lib\net452
