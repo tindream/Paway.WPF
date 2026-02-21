3cebe7dd0db69f861980f6aa0a71014cecb15f70
Tinn <tindream02@126.com>
2026-02-21 21:38:03 +0800

    1.6.2(#856)

--- builder/Share.bat
+++ builder/Share.bat
@@ -1,6 +1,6 @@
-mklink /j Paway.WPF.2.0.1_net45							C:\Users\Tinn\.nuget\packages\paway.wpf\2.0.1\lib\net45
-mklink /j Paway.WPF.2.0.1_net48							C:\Users\Tinn\.nuget\packages\paway.wpf\2.0.1\lib\net48
-mklink /j Paway.WPF.2.0.1_net6.0-windows				C:\Users\Tinn\.nuget\packages\paway.wpf\2.0.1\lib\net6.0-windows
+mklink /j Paway.WPF.1.6.2_net45							C:\Users\Tinn\.nuget\packages\paway.wpf\1.6.2\lib\net45
+mklink /j Paway.WPF.1.6.2_net48							C:\Users\Tinn\.nuget\packages\paway.wpf\1.6.2\lib\net48
+mklink /j Paway.WPF.1.6.2_net6.0-windows				C:\Users\Tinn\.nuget\packages\paway.wpf\1.6.2\lib\net6.0-windows
 
-mklink /j Paway.Model.2.0.1_net452						C:\Users\Tinn\.nuget\packages\paway.model\2.0.1\lib\net452
-mklink /j Paway.Model.2.0.1_net48						C:\Users\Tinn\.nuget\packages\paway.model\2.0.1\lib\net48
+mklink /j Paway.Model.1.6.2_net452						C:\Users\Tinn\.nuget\packages\paway.model\1.6.2\lib\net452
+mklink /j Paway.Model.1.6.2_net48						C:\Users\Tinn\.nuget\packages\paway.model\1.6.2\lib\net48
