0f63e76627d6d3b872dd216c737c0565f67f07e3
Tinn <tindream02@126.com>
2026-06-15 10:45:02 +0800

    1.6.5(#870)

--- builder/Share.bat
+++ builder/Share.bat
@@ -1,7 +1,7 @@
-mklink /j Paway.WPF.1.6.4_net45							C:\Users\Tinn\.nuget\packages\paway.wpf\1.6.4\lib\net45
-mklink /j Paway.WPF.1.6.4_net48							C:\Users\Tinn\.nuget\packages\paway.wpf\1.6.4\lib\net48
-mklink /j Paway.WPF.1.6.4_net6.0-windows				C:\Users\Tinn\.nuget\packages\paway.wpf\1.6.4\lib\net6.0-windows
+mklink /j Paway.WPF.1.6.5_net462						C:\Users\Tinn\.nuget\packages\paway.wpf\1.6.5\lib\net462
+mklink /j Paway.WPF.1.6.5_net48							C:\Users\Tinn\.nuget\packages\paway.wpf\1.6.5\lib\net48
+mklink /j Paway.WPF.1.6.5_net6.0-windows				C:\Users\Tinn\.nuget\packages\paway.wpf\1.6.5\lib\net6.0-windows
 
-mklink /j Paway.Model.1.6.4_net462						C:\Users\Tinn\.nuget\packages\paway.model\1.6.4\lib\net462
-mklink /j Paway.Model.1.6.4_net48						C:\Users\Tinn\.nuget\packages\paway.model\1.6.4\lib\net48
-mklink /j Paway.Model.1.6.4_net6.0-windows				C:\Users\Tinn\.nuget\packages\paway.model\1.6.4\lib\net6.0-windows
+mklink /j Paway.Model.1.6.5_net462						C:\Users\Tinn\.nuget\packages\paway.model\1.6.5\lib\net462
+mklink /j Paway.Model.1.6.5_net48						C:\Users\Tinn\.nuget\packages\paway.model\1.6.5\lib\net48
+mklink /j Paway.Model.1.6.5_net6.0-windows				C:\Users\Tinn\.nuget\packages\paway.model\1.6.5\lib\net6.0-windows
